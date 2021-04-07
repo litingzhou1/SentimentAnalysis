72fde476b28a5b64ad8a878056bc7a712b57749a
Liting Zhou <beckyzoe90@gmail.com>
2021-04-07 16:25:08 +0100

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -5,7 +5,8 @@ from transformers import BertPreTrainedModel, BertModel, AlbertPreTrainedModel, 
 class BertForSentimentClassification(BertPreTrainedModel):
 	def __init__(self, config):
 		super().__init__(config)
-		self.bert = BertModel(config)
+		self.bert = BertModel.from_pretrained('bert-base-uncased')
+		#self.bert = BertModel(config)
 		# self.dropout = nn.Dropout(config.hidden_dropout_prob)
 		#The classification layer that takes the [CLS] representation and outputs the logit
 		self.cls_layer = nn.Linear(config.hidden_size, 1)
@@ -18,7 +19,8 @@ class BertForSentimentClassification(BertPreTrainedModel):
 			(where B is the batch size and T is the input length)
 		'''
 		#Feed the input to Bert model to obtain contextualized representations
-		reps, _ = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+		#reps, _ = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+		reps = self.bert(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state
 		#Obtain the representations of [CLS] heads
 		cls_reps = reps[:, 0]
 		# cls_reps = self.dropout(cls_reps)
